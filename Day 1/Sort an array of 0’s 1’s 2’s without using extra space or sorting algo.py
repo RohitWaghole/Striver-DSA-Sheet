0393bf1b02aa1f1a294f1ef8e3115b262d43cb77
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-25 11:42:21 +0530

    Update Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.py

--- Day 1/Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.py
+++ Day 1/Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.py
@@ -1,32 +1,32 @@
-# class Solution:
-#     def sortColors(self, nums: List[int]) -> None:
-#         nums.sort()
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        nums.sort()
 
 #########################################################################################################
 
-# class Solution:
-#     def sortColors(self, nums: List[int]) -> None:
-#         zero,one,two = 0,0,0
-#         for i in nums:
-#             if i==0:
-#                 zero+=1
-#             elif i==1:
-#                 one+=1
-#             else:
-#                 two+=1
-#         i=0
-#         while zero:
-#             nums[i]=0
-#             i+=1
-#             zero-=1
-#         while one:
-#             nums[i]=1
-#             i+=1
-#             one-=1
-#         while two:
-#             nums[i]=2
-#             i+=1
-#             two-=1
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        zero,one,two = 0,0,0
+        for i in nums:
+            if i==0:
+                zero+=1
+            elif i==1:
+                one+=1
+            else:
+                two+=1
+        i=0
+        while zero:
+            nums[i]=0
+            i+=1
+            zero-=1
+        while one:
+            nums[i]=1
+            i+=1
+            one-=1
+        while two:
+            nums[i]=2
+            i+=1
+            two-=1
 
 #########################################################################################################
 
